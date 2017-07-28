d0c37d3cb690664d452eda4192bf93235ef0cd0b
CORWIN\Corwin75 <malboro1969@gmail.com>
2017-07-29 00:02:33 +0200

    + Create VS C# lib project

---
+++ C#/Cloudflare-Bypass/Cloudflare_Bypass.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cloudflare_Bypass
+{
+    public class Cloudflare_Bypass
+    {
+    }
+}
